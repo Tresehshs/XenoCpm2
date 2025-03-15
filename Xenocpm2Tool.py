1481f8413cfe600cf40dfc383a32520e41987525
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-03-15 20:50:56 +0800

    Update Xenocpm2Tool.py

--- Xenocpm2Tool.py
+++ Xenocpm2Tool.py
@@ -3,7 +3,7 @@ import random
 import requests
 from time import sleep
 import os, signal, sys
-import pyfiglet and figlet_format
+import pyfiglet from figlet_format
 from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
