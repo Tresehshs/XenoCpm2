19755b1cc17169785e3cc6e24ef0e8dcfd181f24
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-03-15 20:34:32 +0800

    Update thein.py

--- thein.py
+++ thein.py
@@ -1,16 +1,6 @@
 import requests
 
-# Copyright (C) Anasov <me@anasov.ly> - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Anasov <me@anasov.ly>, 05, May, 2024.
-
-#############################################
-# DO NOT BUY THIS TOOL FROM ANY SCAMMER !!! #
-# OFFICIAL SELLER IS 'ANAS' AT TELEGRAM !!! #
-#############################################
-
-BASE_URL: str = "https://cpmayanshoppp.squareweb.app/api"
+BASE_URL: str = "https://xeno2.squareweb.app/api"
 
 class Thein:
 
